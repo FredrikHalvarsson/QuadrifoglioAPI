3db44dfedea9b44c9c2835d847ebf4611036fa30
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2024-05-14 13:00:18 +0200

    add productsController

---
+++ Controllers/IngredientsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuadrifoglioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+    }
+}
